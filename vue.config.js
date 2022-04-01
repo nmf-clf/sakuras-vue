f866ebe045eab80b4e7db539ab0906cf531b5fc1
niumengfei <870424431@qq.com>
2022-04-01 19:08:51 +0800

    replace index page

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
+/*
+ * @Author: niumengfei
+ * @Date: 2022-04-01 18:25:27
+ * @LastEditors: niumengfei
+ * @LastEditTime: 2022-04-01 18:40:44
+ */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
 })
